182bf2933e4440078edb2d8779d56de00ce54f2d
Jamu Kakar <jkakar@kakar.ca>
2015-03-12 15:07:37 -0700

    Cleanup programming sections.

--- emacs
+++ emacs
@@ -2,8 +2,9 @@
 
 (setq load-path (cons "~/.emacs.d/plugins" load-path))
 (setq load-path (cons "~/.emacs.d/local" load-path))
-(setq load-path (cons "/usr/local/go/misc/emacs" load-path))
-(setq load-path (cons "/usr/local/Cellar/go//1.2.1/libexec/misc/emacs" load-path))
+(setenv "GOPATH" (expand-file-name "~"))
+(setenv "PATH" (concat (getenv "PATH") ":" (expand-file-name "~/bin")))
+(setq exec-path (append (list (expand-file-name "~/bin") "/usr/local/Cellar/go/1.4.2/bin") exec-path))
 
 ;; Standard packages
 
@@ -16,28 +17,18 @@
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
+;; Local packages
+(add-to-list 'load-path "~/src/github.com/dougm/goflymake")
+
 ;; Plugins
 
-(require 'python-outline)
 (load-library "markdown-mode.el")
 (load-library "highlight-80+.el")
 (load-library "less-css-mode.el")
 
-(setenv "GOPATH" (expand-file-name "~"))
-(setenv "PATH" (concat (getenv "PATH") ":" (expand-file-name "~/bin")))
-(setq exec-path (append (list (expand-file-name "~/bin") "/usr/local/go/bin") exec-path))
-(setq gofmt-command "goimports")
-(add-to-list 'load-path (expand-file-name "~/src/github.com/golang/lint/misc/emacs"))
-(add-to-list 'load-path "~/src/github.com/dougm/goflymake")
 (require 'go-flymake)
-(require 'golint)
-
-;;(add-hook 'after-init-hook 'my-after-init-hook)
-;;(defun my-after-init-hook ()
-;;  (require 'edts-start))
+(require 'elixir-mode)
 
 (require 'appearance)
 (require 'interaction)
 (require 'programming)
-
-(require 'elixir-mode)
--- emacs.d/local/programming.el
+++ emacs.d/local/programming.el
@@ -2,59 +2,15 @@
 
 ;; Go
 
-;; FIXME Figure out how to pass other local .go files to 6g to avoid
-;; spurious errors.
-
-;; (defvar go-compiler "6g")
-
-;; (defun flymake-go-init ()
-;;   (let* ((temp-file (flymake-init-create-temp-buffer-copy
-;;                      'flymake-create-temp-inplace))
-;;          (local-file (file-relative-name
-;;                       temp-file
-;;                       (file-name-directory buffer-file-name))))
-;;     (list go-compiler (list "-o" "/dev/null" temp-file))))
-
-;; (push '(".+\\.go$" flymake-go-init) flymake-allowed-file-name-masks)
-;; (add-hook 'go-mode-hook 'flymake-mode)
-
-;; (setq go-mode-hook
-;;       '(lambda () "Defaults for Go mode." (setq fill-column 78
-;; 												tab-width 4
-;; 												indent-tabs-mode 0)))
-;; (add-hook 'go-mode-hook 'highlight-80+-mode)
-;; (setenv "PATH" (concat (getenv "PATH") ":/usr/local/go/bin"))
-;; (setq exec-path (append exec-path '("/usr/local/go/bin")))
-;; (add-hook 'before-save-hook 'gofmt-before-save)
+(setq gofmt-command "goimports")
+(add-hook 'before-save-hook 'gofmt-before-save)
 
 
 ;; Python
 
-;; (when (load "flymake" t)
-;;   (defun flymake-pyflakes-init ()
-;;     (let* ((temp-file (flymake-init-create-temp-buffer-copy
-;;                        'flymake-create-temp-inplace))
-;;            (local-file (file-relative-name
-;;                         temp-file
-;;                         (file-name-directory buffer-file-name))))
-;;       (list "/Users/jkakar/.emacs.d/plugins/epylint.py" (list local-file))))
-;;   (add-to-list 'flymake-allowed-file-name-masks
-;;                '("\\.py\\'" flymake-pyflakes-init))
-;;   (add-to-list 'flymake-allowed-file-name-masks
-;;                '("\\.tac\\'" flymake-pyflakes-init)))
-
-;; (defun python-outline-settings ()
-;;   (outline-setup "^class \\|[ 	]*def \\|^#"))
-
-;; (defun my-flymake-find-file-hook ()
-;;   (if (file-writable-p buffer-file-name)
-;;       (flymake-find-file-hook)))
-
 (defun unset-python-newline-and-indent ()
   (local-unset-key [?\C-j]))
 
-;; (add-hook 'find-file-hook 'my-flymake-find-file-hook)
-;; (add-hook 'python-mode-hook 'python-outline-settings)
 (add-hook 'python-mode-hook
       '(lambda () "Defaults for Python mode." (setq fill-column 78
                                                     python-guess-indent nil
@@ -62,6 +18,7 @@
 (add-hook 'python-mode-hook 'unset-python-newline-and-indent)
 (setq auto-mode-alist (cons '("\\.tac$" . python-mode) auto-mode-alist))
 (setq tags-table-list-default '("~/.etags/twisted" "~/.etags/txamqp"))
+
 
 ;; Ruby
 
