3c1af0ac5269d095905dfd5cce47a18463c3331e
Jamu Kakar <jkakar@stripe.com>
2018-03-17 21:13:19 -0700

    Remove old config.

--- emacs.d/elpa/erlang-20170913.604/erlang-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "erlang" "20170913.604" "Erlang major mode"
-  '((emacs "24.1")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- emacs.d/elpa/hcl-mode-20170107.27/hcl-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "hcl-mode" "20170107.27" "Major mode for Hashicorp" '((emacs "24.3")) :url "https://github.com/syohex/emacs-hcl-mode")
