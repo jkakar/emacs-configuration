6d7dfabc0507d91ee296940e8d3b1ec0348591e7
Jamu Kakar <jkakar@kakar.ca>
2018-03-19 22:01:06 -0700

    Add empty custom.el file.

---
+++ custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(exec-path-from-shell-check-startup-files nil)
+ '(exec-path-from-shell-variables (quote ("PATH" "MANPATH" "GOPATH")))
+ '(gofmt-show-errors nil)
+ '(markdown-hide-urls t)
+ '(package-selected-packages
+   (quote
+    (graphviz-dot-mode go-eldoc go-mode flycheck dumb-jump magit smex counsel swiper ws-butler exec-path-from-shell base16-theme use-package)))
+ '(rust-format-on-save t t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
